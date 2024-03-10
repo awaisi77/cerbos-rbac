d9afe615c713aad4b7c8fd2c9069dcfa224c81e0
mawaisu <captainawaisullah@gmail.com>
2024-03-10 23:16:25 +0500

    final comit

---
+++ start.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+docker run -i -t -p 3592:3592 \
+  -v $(pwd)/config:/config \
+  -v $(pwd)/policies:/policies \
+  ghcr.io/cerbos/cerbos:0.12.0 \
+  server --config=/config/conf.yaml
